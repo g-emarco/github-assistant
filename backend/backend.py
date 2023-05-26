3fd6313969bd7320a843f280d734df1a4809c15d
emarco <emarco@google.com>
2023-05-26 09:44:35 +0300

    added goolgle support

--- backend/backend.py
+++ backend/backend.py
@@ -2,7 +2,8 @@ import os
 from typing import Any, Dict, List
 
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import VertexAIEmbeddings
+from langchain.chat_models import ChatOpenAI, ChatGooglePalm
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 import pinecone
@@ -21,6 +22,27 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
         index_name=os.environ["PINECONE_INDEX_NAME"],
     )
     chat = ChatOpenAI(
+        verbose=True,
+        temperature=0,
+    )
+
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
+    )
+    return qa({"question": query, "chat_history": chat_history})
+
+
+def run_g_llm(query: str, chat_history: List[Dict[str, Any]] = []):
+    os.environ[
+        "GOOGLE_APPLICATION_CREDENTIALS"
+    ] = "/home/user/github-assistant/vertex-sa-key.json"
+    embeddings = VertexAIEmbeddings()  # Dimention 768
+
+    docsearch = Pinecone.from_existing_index(
+        embedding=embeddings,
+        index_name=os.environ["PINECONE_INDEX_NAME"],
+    )
+    chat = ChatGooglePalm(
         verbose=True,
         temperature=0,
     )
--- ingestion.py
+++ ingestion.py
@@ -2,14 +2,14 @@ import os
 
 from langchain.document_loaders import GitLoader
 from git import Repo
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, VertexAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
 import pinecone
 
 pinecone.init(
-    api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+    api_key=os.environ["G_PINECONE_API_KEY"],
+    environment=os.environ["G_PINECONE_ENVIRONMENT_REGION"],
 )
 
 
@@ -44,6 +44,46 @@ def ingest_docs():
     Pinecone.from_documents(
         documents, embeddings, index_name=os.environ["PINECONE_INDEX_NAME"]
     )
+    print("****Loading to vectorestore done ***")
+
+
+def ingest_docs_g():
+    to_path = "./wordblend-ai"
+    repo = Repo.clone_from(
+        "https://github.com/g-emarco/wordblend-ai", to_path=to_path, branch="main"
+    )
+    loader = GitLoader(repo_path=to_path)
+    raw_documents = loader.load()
+
+    print(f"loaded {len(raw_documents)} documents")
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1200,
+        chunk_overlap=150,
+    )
+
+    for doc in raw_documents:
+        source = doc.metadata["source"]
+        cleaned_source = "/".join(source.split("/")[1:])
+        doc.page_content = (
+            "FILE NAME: "
+            + cleaned_source
+            + "\n###\n"
+            + doc.page_content.replace("\u0000", "")
+        )
+
+    documents = text_splitter.split_documents(raw_documents)
+    os.environ[
+        "GOOGLE_APPLICATION_CREDENTIALS"
+    ] = "/Users/emarco/GithubProjects/github-assistant/vertex-sa-key.json"
+
+    embeddings = VertexAIEmbeddings()  # Dimention 768
+    print(f"Going to add {len(documents)} to Pinecone")
+    chunk_size = 5
+    for i in range(0, len(documents), chunk_size):
+        chunked_documents = documents[i : i + chunk_size]
+        Pinecone.from_documents(
+            chunked_documents, embeddings, index_name=os.environ["PINECONE_INDEX_NAME"]
+        )
     print("****Loading to vectorestore done ***")
 
 
