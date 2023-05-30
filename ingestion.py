dbd7d71cd97e80b1f42536093a93abd9ef55e1d1
emarco <emarco@google.com>
2023-05-30 22:23:52 +0300

    refactored ingestion

--- ingestion.py
+++ ingestion.py
@@ -13,50 +13,13 @@ pinecone.init(
 )
 
 
-def ingest_docs():
-    to_path = "./wordblend-ai"
-    repo = Repo.clone_from(
-        "https://github.com/g-emarco/wordblend-ai", to_path=to_path, branch="main"
-    )
-    loader = GitLoader(repo_path=to_path)
-    raw_documents = loader.load()
-
-    print(f"loaded {len(raw_documents)} documents")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1200,
-        chunk_overlap=150,
-    )
-
-    for doc in raw_documents:
-        source = doc.metadata["source"]
-        cleaned_source = "/".join(source.split("/")[1:])
-        doc.page_content = (
-            "FILE NAME: "
-            + cleaned_source
-            + "\n###\n"
-            + doc.page_content.replace("\u0000", "")
-        )
-
-    documents = text_splitter.split_documents(raw_documents)
-
-    embeddings = OpenAIEmbeddings()
-    print(f"Going to add {len(documents)} to Pinecone")
-    Pinecone.from_documents(
-        documents, embeddings, index_name=os.environ["PINECONE_INDEX_NAME"]
-    )
-    print("****Loading to vectorestore done ***")
-
-
-def ingest_docs_g(repo_url: str) -> None:
-    print(f"***** going to clone {repo_url}")
+def ingest_docs(repo_url: str, vendor: str = "google") -> None:
     to_path = "./repo_to_embed"
     repo = Repo.clone_from(repo_url, to_path=to_path, branch="main")
     loader = GitLoader(repo_path=to_path)
     raw_documents = loader.load()
 
     print(f"loaded {len(raw_documents)} documents")
-    print(f"splitting documents to chunks")
-
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1200,
         chunk_overlap=150,
@@ -73,8 +36,13 @@ def ingest_docs_g(repo_url: str) -> None:
         )
 
     documents = text_splitter.split_documents(raw_documents)
-    embeddings = VertexAIEmbeddings()  # Dimention 768
-    print(f"Going to add {len(documents)} chunks to vectorstore")
+
+    print(f"Going to add {len(documents)} to Pinecone")
+    if vendor == "google":
+        embeddings = VertexAIEmbeddings()
+    elif vendor == "openai":
+        embeddings = OpenAIEmbeddings()
+
     chunk_size = 5
     for i in range(0, len(documents), chunk_size):
         print(f"iteration {i}/{len(documents)/chunk_size}...")
@@ -86,4 +54,4 @@ def ingest_docs_g(repo_url: str) -> None:
 
 
 if __name__ == "__main__":
-    ingest_docs_g(repo_url="https://github.com/g-emarco/wordblend-ai")
+    ingest_docs(repo_url="https://github.com/g-emarco/wordblend-ai")
